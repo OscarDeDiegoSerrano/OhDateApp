ac4605b927c2b02974a6700853e6da51d5806fc9
OscarDeDiegoSerrano <oscarde_diego@ceroca.cat>
2024-04-19 19:24:17 +0200

    Redireccionament de pantalla de chats y matches

--- lib/paginas/pantalla_ChatsMatch.dart
+++ lib/paginas/pantalla_ChatsMatch.dart
@@ -1,89 +1,102 @@
 import 'package:flutter/material.dart';
 import 'package:ohdate_app/paginas/inicio.dart';
+import 'package:ohdate_app/paginas/pantalla_chat.dart';
 
 class PantallaChatsMatch extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Contactos'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Stack(
         children: [
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Image.asset('assets/LogoOhDate.jpg'),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 100,
+                child: Image.asset(
+                  'lib/imatges/LogoOhDate.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const PaginaChat(emailAmbQuiParlem: '', idReceptor: '',)),
+                          );
+                        },
+                        child: Container(
+                          color: const Color.fromARGB(255, 247, 150, 230),
+                          child: const Center(
+                            child: Text(
+                              'Conversaciones',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Expanded(
+                      child: Container(
+                        color: const Color.fromARGB(255, 119, 231, 123),
+                        child: const Center(
+                          child: Text(
+                            'Matches',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Color.fromARGB(255, 247, 150, 230),
-                    child: const Center(
-                      child: Text(
-                        'Conversaciones',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: BottomAppBar(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.chat),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => PantallaChatsMatch()),
+                      );
+                    },
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: const Color.fromARGB(255, 119, 231, 123),
-                    child: const Center(
-                      child: Text(
-                        'Matches',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
+                  IconButton(
+                    icon: const Icon(Icons.home),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => PaginaInicio()),
+                      );
+                    },
                   ),
-                ),
-                BottomAppBar(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.chat),
-                        onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => PantallaChatsMatch()),
-                        );
-                      },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.home),
-                        onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => PaginaInicio()),
-                        );
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.settings),
-                        onPressed: () {
-                          // Acción al presionar el botón de ajustes
-                        },
-                      ),
-                    ],
+                  IconButton(
+                    icon: const Icon(Icons.settings),
+                    onPressed: () {
+                      // Acción al presionar el botón de ajustes
+                    },
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
