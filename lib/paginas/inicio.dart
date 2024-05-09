f8a28dfcfd0f6e1d6ee5e74b3d450d677f8dcdf5
Oscar <oscarde_diego@ceroca.cat>
2024-05-09 18:01:07 +0200

    Container de Swipe amb usuaris reals

--- lib/paginas/inicio.dart
+++ lib/paginas/inicio.dart
@@ -1,5 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:ohdate_app/servicios/servicio_autentificacion.dart';
 import 'package:ohdate_app/paginas/ModificarDatosUsuario.dart';
 import 'package:ohdate_app/paginas/login.dart';
@@ -12,11 +14,28 @@ class PaginaInicio extends StatefulWidget {
 }
 
 class _PaginaInicioState extends State<PaginaInicio> {
+  User? usuarioActual = FirebaseAuth.instance.currentUser;
+  late String nombre;
+
+  @override
+  void initState() {
+    super.initState();
+    // Obtener el nombre del usuario actual
+    if (usuarioActual != null) {
+      obtenerNombreUsuario();
+    }
+  }
+
+  // Función para obtener el nombre del usuario desde Firestore
+  Future<void> obtenerNombreUsuario() async {
+    DocumentSnapshot document = await FirebaseFirestore.instance.collection('usuarios').doc(usuarioActual!.uid).get();
+    setState(() {
+      nombre = document['nombre'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    User? usuarioActual = ServicioAutenticacion().getUsuariActual();
-    String nombre = usuarioActual?.email ?? 'Invitado';
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -81,28 +100,28 @@ class _PaginaInicioState extends State<PaginaInicio> {
                 IconButton(
                   icon: Icon(Icons.chat),
                   onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => PantallaChatsMatch()),
-                  );
-                },
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PantallaChatsMatch()),
+                    );
+                  },
                 ),
                 IconButton(
                   icon: Icon(Icons.home),
                   onPressed: () {
                     Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => PaginaInicio()),
-                  );
+                      context,
+                      MaterialPageRoute(builder: (context) => PaginaInicio()),
+                    );
                   },
                 ),
                 IconButton(
                   icon: Icon(Icons.settings),
                   onPressed: () {
                     Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => PreferenciaBusqueda()),
-                  );
+                      context,
+                      MaterialPageRoute(builder: (context) => PreferenciaBusqueda()),
+                    );
                   },
                 ),
               ],
@@ -120,34 +139,59 @@ class SwipeCardPage extends StatefulWidget {
 }
 
 class _SwipeCardPageState extends State<SwipeCardPage> {
-  List<String> photos = [
-    'lib/imatges/imagen1.jpg',
-    'lib/imatges/imagen2.jpg',
-    'lib/imatges/imagen3.jpg'
-  ];
+  List<Map<String, dynamic>> usersData = [];
   int currentPhotoIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Cargar los datos de usuarios cuando se inicia el widget
+    loadUsers();
+  }
+
+  Future<void> loadUsers() async {
+    CollectionReference users = FirebaseFirestore.instance.collection('usuarios');
+    QuerySnapshot querySnapshot = await users.get();
+    querySnapshot.docs.forEach((doc) {
+      usersData.add({
+        'name': doc['nombre'] ?? '', // Obtener el nombre del usuario, asegurándose de que no sea nulo
+        'photoUrl': '', // Inicialmente, establecer la URL de la foto del usuario como vacía
+        'uid': doc.id, // Obtener el UID del usuario
+      });
+    });
+
+    // Obtener las URLs de las imágenes de los usuarios
+    await getUsersPhotoUrls();
+  }
+
+  Future<void> getUsersPhotoUrls() async {
+    for (int i = 0; i < usersData.length; i++) {
+      // Obtener la URL de la imagen del usuario
+      String photoUrl = await FirebaseStorage.instance.ref().child('${usersData[i]['uid']}/avatar/${usersData[i]['uid']}').getDownloadURL();
+      // Actualizar la URL de la foto del usuario en la lista
+      setState(() {
+        usersData[i]['photoUrl'] = photoUrl;
+      });
+    }
+  }
 
   void swipeLeft() {
     setState(() {
-      currentPhotoIndex = (currentPhotoIndex + 1) % photos.length;
+      currentPhotoIndex = (currentPhotoIndex + 1) % usersData.length;
     });
   }
 
   void swipeRight() {
     setState(() {
-      currentPhotoIndex = (currentPhotoIndex - 1 + photos.length) % photos.length;
+      currentPhotoIndex = (currentPhotoIndex - 1 + usersData.length) % usersData.length;
     });
   }
 
   @override
-   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onPanUpdate: (details) {
-        // Define una sensibilidad personalizada para el swipe
         final sensitivity = 20.0;
-
-        // Si el cambio en la coordenada X es mayor que la sensibilidad definida
         if (details.delta.dx.abs() > sensitivity) {
           if (details.delta.dx > 0) {
             swipeRight();
@@ -167,10 +211,30 @@ class _SwipeCardPageState extends State<SwipeCardPage> {
           child: SizedBox(
             width: double.infinity,
             height: double.infinity,
-            child: Image.asset(
-              '${photos[currentPhotoIndex]}',
-              fit: BoxFit.cover,
-            ),
+            child: usersData.isEmpty
+                ? Center(child: CircularProgressIndicator()) // Indicador de carga mientras se cargan los datos
+                : Stack(
+                    children: [
+                      Image.network(
+                        usersData[currentPhotoIndex]['photoUrl'],
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        left: 10,
+                        child: Text(
+                          usersData[currentPhotoIndex]['name'],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
           ),
         ),
       ),
